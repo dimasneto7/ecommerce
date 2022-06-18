e4e97f2e1b00dbc3ac6bdc773cb9dd6398848842
Dimas Neto <dimas_fcn@outlook.com>
2022-06-18 20:25:10 -0300

    first

---
+++ app/Http/Controllers/ProdutoController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ProdutoController extends Controller
+{
+    public function index(Request $request) {
+        $data = [];
+
+        // Buscar todos os produtos
+        // Tipo... select * from produtos
+        $listaProdutos = \App\Models\Produto::all();
+        $data["lista"] = $listaProdutos;
+
+        return view("home", $data);
+    }
+
+    public function categoria(Request $request) {
+        $data = [];
+
+        return view("categoria", $data);
+    }
+
+}
---
+++ routes/web.php
@@ -0,0 +1,19 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProdutoController;
+use App\Http\Controllers\ClienteController;
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::match(['get', 'post'], '/', [ ProdutoController::class, 'index'])->name('home');
+Route::match(['get', 'post'], '/categoria', [ ProdutoController::class, 'categoria'])->name('categoria');
+Route::match(['get', 'post'], '/cadastrar', [ ClienteController::class, 'cadastrar'])->name('cadastrar');
